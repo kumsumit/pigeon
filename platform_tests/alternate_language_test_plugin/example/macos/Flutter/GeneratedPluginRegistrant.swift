9cca8e8b0a04701d2e89ef716bdcea0e981a784d
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-09-08 19:58:14 +0530

    first commit

---
+++ platform_tests/alternate_language_test_plugin/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import alternate_language_test_plugin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AlternateLanguageTestPlugin.register(with: registry.registrar(forPlugin: "AlternateLanguageTestPlugin"))
+}
---
+++ platform_tests/test_plugin/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import test_plugin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  TestPlugin.register(with: registry.registrar(forPlugin: "TestPlugin"))
+}
